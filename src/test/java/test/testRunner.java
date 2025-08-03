b899033f95b86dbb44c592ae46a52f3679770bdd
Vignesh Vellingiri <vignesh.vellingiri28@gmail.com>
2025-08-03 16:32:02 +0530

    cucumber parrallel and importent plugins are installed

--- src/test/java/test/testRunner.java
+++ src/test/java/test/testRunner.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.junit.runner.RunWith;
 import org.testng.annotations.DataProvider;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
@@ -10,7 +11,6 @@ dryRun =false,monochrome = true,
 plugin = {"pretty","html:target/cucmber_report.html"})
 
 public class testRunner extends AbstractTestNGCucumberTests{
-
 	@Override
 	@DataProvider(parallel = true)
 	public Object[][] scenarios(){
